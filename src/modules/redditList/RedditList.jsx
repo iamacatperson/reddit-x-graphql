aeb063b99159afc5a0e21cad5b949788ba187eb1
Ava Guerrero <ava.square@gmail.com>
2019-03-03 21:25:52 +0800

    feat: Create RedditList and RedditItem components

--- src/modules/redditList/RedditList.jsx
+++ src/modules/redditList/RedditList.jsx
@@ -1,25 +1,62 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from 'prop-types';
+
+import RedditItem from "./redditItem/RedditItem.jsx";
 
 import "./RedditList.scss";
 
-class RedditList extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {};
+export default function RedditList(props) {
 
-	}
+	const { subredditsList, subredditTitle } = props;
 
-	render() {
+	if(subredditsList) {
+		if(subredditsList.length === 0) {
+			return (
+				<div className="reddit-list">
+					<p className="reddit-list__no-result">No results found for this category.</p>
+				</div>
+			);
+		} else {
+			return (
+				<div className="reddit-list">
+					
+					<ul className="reddit-list__container">
+						{subredditsList.map((subReddit, index) => {
+							return (
+								<RedditItem 
+									key={index}
+									title={subReddit.title}
+									url={subReddit.url}
+									numComments={subReddit.numComments}
+									author={subReddit.author.username}
+									score={subReddit.score}
+								/>
+							);
+						})}
+					</ul>
 
+					<p className="reddit-list__footer">Only showing 50 results at most. Visit the <a href={`http://www.reddit.com/r/${subredditTitle}`} target="_blank" rel="noopener noreferrer">r/{subredditTitle}</a> page to continue reading!</p>
+
+				</div>
+			);
+		}
+	} else {
 		return (
-			<div className="reddit-list">
-				
-				The List
-
-			</div>
+			<ul className="reddit-list__container">
+				{[1, 2, 3].map((item, index) => {
+					return (
+						<li key={index} className="reddit-list__item-loading">
+							<div className="reddit-list__item-loading-title"></div>
+							<div className="reddit-list__item-loading-body"></div>
+						</li>
+					);
+				})}
+			</ul>
 		);
-
-	}
+	}		
 }
 
-export default RedditList;
+RedditList.propTypes = {
+  subredditsList: PropTypes.array,
+  subredditTitle: PropTypes.string
+};
---
+++ src/modules/redditList/redditItem/RedditItem.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+import "./RedditItem.scss";
+
+export default function RedditItem(props) {
+
+	const { title, score, url, author, numComments } = props;
+
+	function kFormatter(num) {
+	    return num > 999 ? (num/1000).toFixed(1) + 'k' : num;
+	}
+
+	return (
+		<li className="reddit-item">
+			<div className="reddit-item__container">
+
+				<div className="row">
+					<div className="col-2 col-md-1 col-lg-1">
+						<div className="reddit-item__score-container">
+							<div className="reddit-item__score"><span>{kFormatter(score)}</span></div>
+						</div>
+					</div>
+					<div className="col-10 col-md-11 col-lg-11">
+						<p className="reddit-item__author">Posted by: {author}</p>
+						<h3><a href={url} target="_blank" rel="noopener noreferrer">{title}</a></h3>
+
+						<p className="reddit-item__num-comments">{numComments} comments</p>	
+					</div>
+				</div>
+			</div>
+		</li>
+	);
+}
+
+RedditItem.propTypes = {
+	title: PropTypes.string.isRequired,
+	score: PropTypes.number.isRequired,
+	url: PropTypes.string.isRequired,
+	author: PropTypes.string.isRequired,
+	numComments: PropTypes.number.isRequired
+};
