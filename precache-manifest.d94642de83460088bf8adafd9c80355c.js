553910ffa905097ca331b1eb9773ed7f693d9e63
Ava <ava@codigo.sg>
2019-03-04 13:55:39 +0800

    Updates

--- precache-manifest.d94642de83460088bf8adafd9c80355c.js
+++ precache-manifest.d94642de83460088bf8adafd9c80355c.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/reddit-x-graphql/static/js/runtime~main.71b88f24.js"
   },
   {
-    "revision": "ce5051bc926c5b0576f0",
-    "url": "/reddit-x-graphql/static/js/main.ce5051bc.chunk.js"
+    "revision": "b9044ee4624c80fa1c7c",
+    "url": "/reddit-x-graphql/static/js/main.b9044ee4.chunk.js"
   },
   {
     "revision": "1889436ee7d39d7462ff",
     "url": "/reddit-x-graphql/static/js/2.1889436e.chunk.js"
   },
   {
-    "revision": "ce5051bc926c5b0576f0",
-    "url": "/reddit-x-graphql/static/css/main.1d2f6552.chunk.css"
+    "revision": "b9044ee4624c80fa1c7c",
+    "url": "/reddit-x-graphql/static/css/main.2b632a3b.chunk.css"
   },
   {
     "revision": "1889436ee7d39d7462ff",
     "url": "/reddit-x-graphql/static/css/2.977f7e2f.chunk.css"
   },
   {
-    "revision": "4ead344ff20d6882fd313b782d80c772",
+    "revision": "703ef09a1f6bf3d05244fdc65701f9d4",
     "url": "/reddit-x-graphql/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/reddit-x-graphql/precache-manifest.7cdbe68bc0a2677741fe638c35db4541.js"
+  "/reddit-x-graphql/precache-manifest.d94642de83460088bf8adafd9c80355c.js"
 );
 
 workbox.clientsClaim();
